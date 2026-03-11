07374dbd79329fe9d4a503a63154fe97e94519a0
Brindha666 <brindhaadevij@gmail.com>
2026-03-11 10:49:17 +0530

    datatypes

---
+++ Chrome.java
@@ -0,0 +1,29 @@
+package org.testleaf.week1;
+
+public class Chrome {
+
+    public static void main(String[] args) {
+        
+        float a = 91.0f;
+        System.out.println(a);
+
+        String b= "Google";
+        System.out.println(b);
+
+        Boolean isbeta= false;
+        System.out.println(isbeta);
+
+        int year=2008;
+        System.out.println(year);
+
+        char value='c';
+        System.out.println(value);
+
+
+
+
+
+
+    }
+
+}
