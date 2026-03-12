be6be02aa4f218c2be4aa7405c50c8ddaf718f0c
Brindha666 <brindhaadevij@gmail.com>
2026-03-12 20:54:30 +0530

    Add files via upload

---
+++ findoutodd.java
@@ -0,0 +1,19 @@
+package org.testleaf.week1.day2;
+
+public class findoutodd {
+
+    
+
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 10; a++) {
+
+            if (a % 2 == 0) {
+                System.out.println(a + " is an Even number");
+            } else {
+                System.out.println(a + " is an Odd number");
+            }
+
+        }
+    }
+}
