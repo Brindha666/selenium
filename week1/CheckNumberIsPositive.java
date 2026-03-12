43527b554baaf7d72ad77589a7d34c4ac77850b6
Brindha666 <brindhaadevij@gmail.com>
2026-03-12 17:04:10 +0530

    Add files via upload
    conditional statements

---
+++ week1/CheckNumberIsPositive.java
@@ -0,0 +1,34 @@
+package org.testleaf.week1.day2;
+
+public class CheckNumberIsPositive {
+
+
+    public static void main(String[] args)
+
+    {
+
+    int version=10;
+
+
+if(version>0)
+{
+    System.out.println("The number is positive");
+
+}
+
+else if(version <0)
+
+
+    {
+    System.out.println("The number is negative");
+
+}
+
+else {
+            System.out.println("The given number is Zero");
+        }
+
+}
+
+}
+
