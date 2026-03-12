8f75dc2a8a72153f7ea174667a0cf3f17659ae35
Brindha666 <brindhaadevij@gmail.com>
2026-03-12 20:56:26 +0530

    Add files via upload

---
+++ week1/fibonacciseries.java
@@ -0,0 +1,32 @@
+package org.testleaf.week1.day2;
+
+public class fibonacciseries {
+
+    public static void main(String[] args) {
+        
+
+        
+        
+        int b=0, c=1,d=1;
+
+        System.out.println (b);
+
+        for(int i=0;i<8;i++)
+
+        {
+
+        System.out.println(d);
+            d=b+c;
+            b=c;
+            c=d;
+
+          
+        }
+
+        
+
+
+
+    }
+
+}
